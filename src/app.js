2647484917ccfda7f482154b66e3ebc5353d93df
Roberto Sousa <robertosousa186@gmail.com>
2022-03-29 00:45:55 -0300

    Updating app and server

--- src/app.js
+++ src/app.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const routes = require('./routes');
 
 const app = express();
 
 app.use(express.json());
 
-module.exports = app;
+const configureExpress = () => {
+  app.use('/', routes);
+
+  return app;
+};
+
+module.exports = () => {
+  const app = configureExpress();
+
+  return app;
+};
--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
-const app = require('./app');
+const setupApp = require('./app');
 
 try {
+  const app = setupApp();
+
   app.listen(3000, () => {
-    console.log(`Servidor rodando na porta 3000`);
+    console.log(`🏁 Servidor rodando na porta http://localhost:3000`);
   });
 } catch (err) {
   console.error(err);
