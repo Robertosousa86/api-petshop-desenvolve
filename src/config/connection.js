36890cc9d3485f821aa7e440a4ba7f9e204ff398
Roberto Sousa <robertosousa186@gmail.com>
2022-03-29 23:34:33 -0300

    Creating connection file

---
+++ src/config/connection.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql');
+require('dotenv').config();
+
+const connection = mysql.createConnection({
+  host: process.env.HOST,
+  port: process.env.DB_PORT,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+});
+
+module.exports = connection;
