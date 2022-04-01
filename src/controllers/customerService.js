cf50abc17e54d76f27a9471bdf5e214a6017bfa3
Roberto Sousa <robertosousa186@gmail.com>
2022-04-01 20:49:09 -0300

    Updating custumerService

--- src/controllers/customerService.js
+++ src/controllers/customerService.js
@@ -1,7 +1,21 @@
+const Attendance = require('../models/attendance');
+
 class CustomerServiceController {
-  get(req, res) {
+  getAttendance(req, res) {
     try {
       return res.send('Você está na rota atendimentos utilizado o método GET');
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  postAttendance(req, res) {
+    try {
+      const attendance = req.body;
+
+      Attendance.insert(attendance);
+
+      return res.send('Post atendimento');
     } catch (err) {
       console.error(err);
     }
