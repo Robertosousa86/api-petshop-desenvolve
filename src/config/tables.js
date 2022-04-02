8b4cdf0ea5f53031fee97e65bf213f906dd92d44
Roberto Sousa <robertosousa186@gmail.com>
2022-04-02 17:58:47 -0300

    Updating config/tables

--- src/config/tables.js
+++ src/config/tables.js
@@ -1,11 +1,12 @@
+require('dotenv').config();
+
 class Tables {
   init(connection) {
     this.connection = connection;
   }
 
   createAttendance() {
-    const sql =
-      'CREATE TABLE IF NOT EXISTS Atendimentos (id int NOT NULL AUTO_INCREMENT, cliente varchar(50) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL, status varchar(20) NOT NULL, observacoes text, PRIMARY KEY(id))';
+    const sql = process.env.CREATE_TABLE;
 
     this.connection.query(sql, (error) => {
       error
