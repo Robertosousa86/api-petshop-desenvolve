1297f0a9ab939029efb079cdfe6081a00389a1f1
Roberto Sousa <robertosousa186@gmail.com>
2022-04-02 18:19:31 -0300

    Updating attendance model

--- src/models/attendance.js
+++ src/models/attendance.js
@@ -2,7 +2,7 @@ const moment = require('moment');
 const connection = require('../config/connection');
 
 class Attendance {
-  insert(attendance) {
+  insert(attendance, res) {
     const creationDate = moment().format('YYYY-MM-DD HH:mm:ss');
     const date = moment(attendance.date, 'DD/MM/YYYY').format(
       'YYYY-MM-DD HH:mm:ss'
@@ -11,8 +11,8 @@ class Attendance {
 
     const sql = 'INSERT INTO Atendimentos SET ?';
 
-    connection.query(sql, datedService, (error, results) => {
-      error ? console.error(error) : console.log(results);
+    connection.query(sql, datedService, (err, data) => {
+      err ? res.status(400).json(err) : res.status(201).json(data);
     });
   }
 }
