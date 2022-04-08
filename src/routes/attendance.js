41577bc1f3297a2f139dc6397376f1019f47f788
Roberto Sousa <robertosousa186@gmail.com>
2022-04-08 00:16:53 -0300

    Creating delete method

--- src/routes/attendance.js
+++ src/routes/attendance.js
@@ -10,5 +10,6 @@ router.get('/', (req, res) => attendanceController.getAttendance(req, res));
 router.get('/:id', (req, res) =>attendanceController.getAttendanceById(req, res));
 router.post('/', (req, res) => attendanceController.postAttendance(req, res));
 router.patch('/:id', (req, res) => attendanceController.patchAttendance(req, res));
+router.delete('/:id', (req, res) => attendanceController.deleteAttendance(req, res))
 
 module.exports = router;
